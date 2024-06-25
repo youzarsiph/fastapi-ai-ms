6cd7855bc01e4bc8b0dca242e7fdd02842f3dee5
Yousef Abu Shanab <josephyousef249@gmail.com>
2024-06-25 09:38:04 +0300

    add github actions workflows and update docs

--- README.md
+++ README.md
@@ -1,3 +1,51 @@
-# fastapi-ai-ms
+# fastapi-hf-hub
 
-FastAPI AI micro service using HuggingFace Hub
+[![Ruff Lint](https://github.com/youzarsiph/fastapi-hf-hub/actions/workflows/ruff.yml/badge.svg)](https://github.com/youzarsiph/fastapi-hf-hub/actions/workflows/ruff.yml)
+[![Black Format](https://github.com/youzarsiph/fastapi-hf-hub/actions/workflows/black.yml/badge.svg)](https://github.com/youzarsiph/fastapi-hf-hub/actions/workflows/black.yml)
+[![Django CI](https://github.com/youzarsiph/fastapi-hf-hub/actions/workflows/fastapi.yml/badge.svg)](https://github.com/youzarsiph/fastapi-hf-hub/actions/workflows/fastapi.yml)
+
+FastAPI AI microservice using HuggingFace Hub
+
+## Get started
+
+Clone the repo:
+
+```console
+git clone https://github.com/youzarsiph/fastapi-hf-hub
+```
+
+Install `poetry`, a Python tool for building and publishing packages:
+
+```console
+python -m pip install poetry
+```
+
+Install dependencies:
+
+```console
+python -m poetry install
+```
+
+Activate virtual environment
+
+```console
+python -m poetry env use python
+```
+
+Create a `.env` file that contains your HuggingFace access token, you may need to create an account on [HuggingFace](https://huggingface.co/):
+
+```bash
+# Your HF access token
+HF_TOKEN=hf_**********************************
+
+```
+
+Or you can export `HF_TOKEN` env variable.
+
+Run the server
+
+```console
+fastapi run fastapi_hf_hub/main.py
+```
+
+Now you are ready to go.
--- fastapi_hf_hub/models.py
+++ fastapi_hf_hub/models.py
@@ -5,52 +5,52 @@ from pydantic import BaseModel
 
 # Create your models here.
 class HFModel(BaseModel):
-    """Adds model option to let the user to select which HF model to use"""
+    """Adds model option to let the user to select which HF model to use."""
 
     model: str | None
 
 
 class ChatCompletionModel(HFModel):
-    """Chat completion model"""
+    """Chat completion model represents ChatCompletion input. Provides messages fields."""
 
     messages: list[dict[str, str]]
 
 
 class QAModel(HFModel):
-    """Question answering model"""
+    """Question answering model represents QA input. Provides context and question fields."""
 
     context: str
     question: str
 
 
 class SentenceSimilarityModel(HFModel):
-    """Sentence similarity model"""
+    """Sentence similarity model represents SentenceSimilarity input. Provides sentence and sentences fields."""
 
     sentence: str
     sentences: list[str]
 
 
 class PromptModel(HFModel):
-    """Prompt model"""
+    """Prompt model represents input for tasks that require a prompt. Provides prompt field."""
 
     prompt: str
 
 
 class TextModel(HFModel):
-    """Text model"""
+    """Text model represents Text input. Provides text field"""
 
     text: str
 
 
 class TranslationModel(TextModel):
-    """Translation model"""
+    """Translation model represents Translation input. Provides source and target fields."""
 
     source: str
     target: str
 
 
 class ZeroShotClassificationModel(TextModel):
-    """Translation model"""
+    """Zero shot classification model represents ZeroShotClassification input. Provides labels and is_multi_label fields."""
 
     labels: list[str]
     is_multi_label: bool
